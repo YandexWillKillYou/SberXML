88da6a8a023bf6a303c49228508946437ce78cba
Artem Maslov <artmmslv@gmail.com>
2019-10-30 16:05:03 +0300

    Now stores results to MySQL database

--- src/just/artmmslv/AttributesFinder.java
+++ src/just/artmmslv/AttributesFinder.java
@@ -3,19 +3,28 @@ package just.artmmslv;
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *  Вывести имена и значения всех атрибутов для par step="1" name="ГРАЖДАНСТВО"
  */
 
-public class AttributesPrinter extends DefaultHandler {
+public class AttributesFinder extends DefaultHandler {
+    private List<String> result = null;
+
+    List<String> getAttributes(){
+        return result;
+    }
     @Override
     public void startElement(String uri,
                              String localName,
                              String qName,
                              Attributes attributes) {
         if("par".equals(qName) && "1".equals(attributes.getValue("step")) && "ГРАЖДАНСТВО".equals(attributes.getValue("name"))) {
+            result = new ArrayList<>();
             for(int i = 0; i < attributes.getLength(); i++){
-                System.out.println(attributes.getQName(i) + " = " + attributes.getValue(i));
+                result.add(attributes.getQName(i) + " = " + attributes.getValue(i));
             }
         }
     }
--- src/just/artmmslv/Main.java
+++ src/just/artmmslv/Main.java
@@ -10,37 +10,42 @@ import javax.xml.parsers.SAXParserFactory;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.*;
 import java.util.List;
 
 /**
- * Решение учебной задачи. Сделано Масловым А.М. 23-24.10.2019 г.
- *
+ * Решение учебной задачи. Сделано Масловым А.М. 23-30.10.2019 г.
+ * <p>
  * Условие:
  * Необходимо сформировать коллекцию, содержащую все виды документов в отсортированном
  * порядке.
  * Вывести имена и значения всех атрибутов для par step="1" name="ГРАЖДАНСТВО"
  * Задача со звездочкой создать в базе таблицу справочник со значениями из первой части
- *
+ * <p>
  * Источник лежит в file.xml
  */
 
 public class Main {
-    //Использовал материал https://docs.oracle.com/javase/tutorial/jaxp/sax/parsing.html
+    //Использовал материалы:
+    //https://docs.oracle.com/javase/tutorial/jaxp/sax/parsing.html
+    //https://www.tutorialspoint.com/jdbc/jdbc-create-database.htm
     public static void main(String[] args) {
-        if(args.length == 0){
-            printList(findDocumentsInXML(Paths.get("file.xml")));
-            System.out.println("______________________________________");
-            printAttributes(Paths.get("file.xml"));
+        Path path;
+        if (args.length == 0) {
+            path = Paths.get("file.xml");
         } else {
-            for(String arg : args){
-                printList(findDocumentsInXML(Paths.get(arg)));
-                printAttributes(Paths.get(arg));
-            }
+            path = Paths.get(args[0]);
         }
-
+        List<String> docs = findDocumentsInXML(path);
+        List<String> attrs = findAttributesInXML(path);
+        printList(docs);
+        System.out.println();
+        printList(attrs);
+        System.out.println();
+        createDatabaseWithData(docs);
     }
 
-    private static List<String> findDocumentsInXML(Path source){
+    private static List<String> findDocumentsInXML(Path source) {
         try {
             SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser saxParser = factory.newSAXParser();
@@ -49,20 +54,74 @@ public class Main {
             xmlReader.setContentHandler(finder);
             xmlReader.parse(source.toString());
             return finder.getDocuments();
-        }catch (ParserConfigurationException | SAXException | IOException e ){
+        } catch (ParserConfigurationException | SAXException | IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void printAttributes(Path source){
-        try{
+    private static List<String> findAttributesInXML(Path source) {
+        try {
             SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser saxParser = factory.newSAXParser();
             XMLReader xmlReader = saxParser.getXMLReader();
-            AttributesPrinter printer = new AttributesPrinter();
-            xmlReader.setContentHandler(printer);
+            AttributesFinder finder = new AttributesFinder();
+            xmlReader.setContentHandler(finder);
             xmlReader.parse(source.toString());
-        }catch (ParserConfigurationException | SAXException | IOException e){
+            return finder.getAttributes();
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Создает БД mysql DATABASE_NAME, в ней таблицу TABLE_NAME с одним атрибутом Name
+     * В нее складывает строки из @param data
+     *
+     * Для корректной работы с БД должна быть установлена и запущена база mysql
+     * https://www.digitalocean.com/community/tutorials/mysql-ubuntu-18-04-ru
+     *
+     * Для работы с кириллическим содержимым @param data нужно изменить конфигурацию /etc/mysql/my.cnf
+     * образец лежит в корне проекта
+     * https://gahcep.github.io/blog/2013/01/05/mysql-utf8/
+     */
+    private static void createDatabaseWithData(List<String> data) {
+        try {
+            final String USER = "root";
+            final String PASS = "password";
+            final String DATABASE_NAME = "DOCUMENTS";
+            final String TABLE_NAME = "DOCUMENTS";
+            final String DB_URL = "jdbc:mysql://localhost/";
+
+            Connection connection;
+            Statement statement;
+
+            //Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            statement = connection.createStatement();
+
+            String createDatabaseQuery = "CREATE DATABASE " + DATABASE_NAME;
+            statement.executeUpdate(createDatabaseQuery);
+
+            String useDatabaseQuery = "USE " + DATABASE_NAME;
+            statement.executeUpdate(useDatabaseQuery);
+
+            String createTableQuery = "CREATE TABLE " + TABLE_NAME + "(Name varchar(255));";
+            statement.executeUpdate(createTableQuery);
+
+            for (String doc : data) {
+                String insertQuery = "INSERT INTO " + TABLE_NAME + " VALUES ('" + doc + "');";
+                statement.executeUpdate(insertQuery);
+            }
+
+            String checkResultQuery = "SELECT * FROM " + TABLE_NAME + ";";
+            ResultSet resultSet = statement.executeQuery(checkResultQuery);
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("Name"));
+            }
+
+
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
